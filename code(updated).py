f71333aa1cf7ffd73b19cd5da6fa9d8ea104ad91
Vipul Gupta <gvipul294@gmail.com>
2018-06-03 00:14:18 +0530

    Update code(updated).py

--- code(updated).py
+++ code(updated).py
@@ -1,43 +1,5 @@
-import pandas as pd
 import math
 import operator
-
-
-df1=pd.read_csv('data/1.csv' ,header=1)
-
-df2=pd.read_csv('data/2.csv' ,header=2)
-
-df2=df2.head(2)
-
-df1=df1.head(10)
-
-"""for i in df1['S.No.'].values:
-    i=int(i)"""
-
-df3=pd.read_csv('data/3.csv' ,header=1)
-
-df4=pd.read_csv('data/4.csv' ,header=1)
-
-"""df5=pd.read_csv('data/5.csv')"""
-
-df3=df3.head(3)
-
-df4=df4.head(3)
-
-correct_ans_set= df2[['Q1.1','Q2.1','Q3.1','Q4.1','Q5.1','Q6.1','Q7.1','Q8.1','Q9.1','Q10.1','Q11.1','Q12.1','Q13.1','Q14.1','Q15.1','Q16.1','Q17.1','Q18.1','Q19.1','Q20.1']]
-
-marks_set= df2[['Q1.2','Q2.2','Q3.2','Q4.2','Q5.2','Q6.2','Q7.2','Q8.2','Q9.2','Q10.2','Q11.2','Q12.2','Q13.2','Q14.2','Q15.2','Q16.2','Q17.2','Q18.2','Q19.2','Q20.2']]
-
-concept_set= df2[['Q1.3','Q2.3','Q3.3','Q4.3','Q5.3','Q6.3','Q7.3','Q8.3','Q9.3','Q10.3','Q11.3','Q12.3','Q13.3','Q14.3','Q15.3','Q16.3','Q17.3','Q18.3','Q19.3','Q20.3']]
-
-df3_set= df3[['Q1','Q2','Q3','Q4','Q5','Q6','Q7','Q8','Q9','Q10','Q11','Q12','Q13','Q14','Q15','Q16','Q17','Q18','Q19','Q20']]
-             
-
-df4_set= df4[['Q1','Q2','Q3','Q4','Q5','Q6','Q7','Q8','Q9','Q10','Q11','Q12','Q13','Q14','Q15','Q16','Q17','Q18','Q19','Q20']]
-
-Ques=['Q1','Q2','Q3','Q4','Q5','Q6','Q7','Q8','Q9','Q10','Q11','Q12','Q13','Q14','Q15','Q16','Q17','Q18','Q19','Q20']
-Ans=['Q1.1','Q2.1','Q3.1','Q4.1','Q5.1','Q6.1','Q7.1','Q8.1','Q9.1','Q10.1','Q11.1','Q12.1','Q13.1','Q14.1','Q15.1','Q16.1','Q17.1','Q18.1','Q19.1','Q20.1']
-
 
 score=[]
 Max_score=[]
@@ -51,8 +13,14 @@ ConceptScoreDataSet = dict(zip(concept_list,score))
 conceptTest1=['ST1', 'ST2', 'ST3', 'ST4', 'ST5']
 conceptTest2=['ST6', 'ST7', 'ST8', 'ST9', 'ST10']
 conceptTest_names=['Concept1 ', 'Concept2 ', 'Concept3 ', 'Concept4 ', 'Concept5 ']
+MisConceptTest_names=['Conpcet1 ', 'Conpcet2 ', 'Concept3 ', 'Concept4 ', 'Concept5 ']
+
 ConceptTest1_heads = dict(zip(conceptTest1,conceptTest_names))
 ConceptTest2_heads = dict(zip(conceptTest2,conceptTest_names))
+
+MisConceptTest1_heads = dict(zip(conceptTest1,MisConceptTest_names))
+MisConceptTest2_heads = dict(zip(conceptTest2,MisConceptTest_names))
+
 
 for j in range(3):
     tempScore=0
@@ -86,9 +54,9 @@ for j in range(3):
                 tempScore=tempScore+marks
                 ConceptScoreDataSet[''.join(concept)] = ConceptScoreDataSet[''.join(concept)]+marks 
                 if(df3['testid'][j]=='test-0001'):
-                    df4['' + ConceptTest1_heads[''.join(concept)] + 'score'][j]= ConceptScoreDataSet[''.join(concept)]
+                    df4['' + MisConceptTest1_heads[''.join(concept)] + 'score'][j]= ConceptScoreDataSet[''.join(concept)]
                 elif (df3['testid'][j]=='test-0002'):
-                    df4['' + ConceptTest2_heads[''.join(concept)] + 'score'][j]= ConceptScoreDataSet[''.join(concept)]
+                    df4['' + MisConceptTest2_heads[''.join(concept)] + 'score'][j]= ConceptScoreDataSet[''.join(concept)]
                 
             else:
                 df4[''.join(i)][j]=0
